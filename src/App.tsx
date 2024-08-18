582eb4dbf7381d9bd5084062d09a1a1bd9cfff84
asnganje <abd.nganje@gmail.com>
2024-08-18 18:22:32 +0300

    Add necessary pages and react router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import { Button } from "./components/ui/button"
+import { RootState } from "./store";
+import { useAppSelector } from "./hooks";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { 
+        HomeLayout, 
+        Landing, 
+        Error,
+        Checkout,
+        Products,
+        SingleProduct,
+        Orders,
+        Cart,
+        About,
+        Login,
+        Register
+      } from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landing />
+  },
+  {
+    path: '/cart',
+    element: <Cart />
+  },
+])
+
 
 function App() {
   return (
-    <div className="text-blue-500">
-      App
-      <Button>Click me</Button>
-    </div>
+    <RouterProvider router={router}/>
   )
 }
 export default App
