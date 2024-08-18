0824a166c9fc0c916ebeb29908ff3dfb2dc52d27
asnganje <abd.nganje@gmail.com>
2024-08-18 17:43:57 +0300

    setup vite app including tailwind and shadcn ui setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div className="text-blue-500">
+      App
+      <Button>Click me</Button>
+    </div>
+  )
+}
+export default App
