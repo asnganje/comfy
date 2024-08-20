265815e4a7f51b288c892654d67a108500334f2b
asnganje <abd.nganje@gmail.com>
2024-08-20 14:54:32 +0300

    Add header element to homelayout

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,6 +1,15 @@
+import { Header } from "@/components"
+import { Outlet } from "react-router-dom"
+
 function HomeLayout() {
   return (
-    <div>HomeLayout</div>
+    <>
+      <Header />
+      <nav>Navbar</nav>
+      <div className="align-element py-20">
+        <Outlet />
+      </div>
+    </>
   )
 }
 export default HomeLayout
