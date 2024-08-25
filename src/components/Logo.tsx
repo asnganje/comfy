78622a61b6a6b185510b68b9b7c9374bf6b56fdb
asnganje <abd.nganje@gmail.com>
2024-08-25 13:26:21 +0300

    Add logic for changing theme in shadcn/ui

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 function Logo() {
   return (
     // remember to change to text-white
-    <Link to="/" className=" hidden lg:flex justify-center items-center p-2 bg-primary rounded-lg text-black">
+    <Link to="/" className=" hidden lg:flex justify-center items-center p-2 bg-primary rounded-lg text-white">
       <Armchair className="w-8 h-8"/>
     </Link>
   )
